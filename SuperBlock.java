c0b154081ed8022b6fa7540645cb04257ceedfcd
Aaron <aaronjameshays@gmail.com>
2019-06-07 14:47:02 -0700

    inode blocks fixed again lol

--- SuperBlock.java
+++ SuperBlock.java
@@ -6,15 +6,17 @@
 
 public class SuperBlock {
 
-    private final int INODE_BLOCKS = 64;    // total no of inode blocks
+    private final int NUM_OF_INODE_BLOCKS = 64;    // total no of inode blocks
     private final int BLOCK_SIZE = Disk.blockSize;  // block size from Disk class
     private final int INODE_LOC = 4;    // location of first inode
     private final int BLOCK_LOC = 0;    // location of superblock
     private final int FREELIST_LOC = 8; // location of freelist
 
-    public int numInodes; //
-    public int numBlocks;
+    public int inodeBlocks;
+    public int totalBlocks;
     public int freeList;
+
+
 
 
 
@@ -22,30 +24,31 @@ public class SuperBlock {
 
         byte[] superBlock = new byte[BLOCK_SIZE];
         SysLib.rawread(BLOCK_LOC, superBlock);
-        numBlocks = SysLib.bytes2int(superBlock, BLOCK_LOC);
-        numInodes = SysLib.bytes2int(superBlock, INODE_LOC);
+        this.totalBlocks = SysLib.bytes2int(superBlock, BLOCK_LOC);
+        inodeBlocks = SysLib.bytes2int(superBlock, INODE_LOC);
         freeList = SysLib.bytes2int(superBlock, FREELIST_LOC);
 
-        if(numBlocks != totalBlocks || numInodes <= 0 || freeList < 2){
-            numBlocks = totalBlocks;
-            format(INODE_BLOCKS);
+
+        if(this.totalBlocks != totalBlocks || inodeBlocks <= 0 || freeList < 2){
+            this.totalBlocks = totalBlocks;
+            format(NUM_OF_INODE_BLOCKS);
             SysLib.cerr("formatting...");
         }
     }
 
     public void format(int blockNum){
 
-        numInodes = blockNum;
+        inodeBlocks = blockNum;
 
-        for (short i = 0; i < numInodes; i++) {
+        for (short i = 0; i < inodeBlocks; i++) {
             Inode tempInode = new Inode();
             tempInode.flag = 0;
             tempInode.toDisk(i);
         }
 
-        freeList = 2 + numInodes / 16;
+        freeList = 2 + inodeBlocks / 16;
 
-        for (int i = freeList; i < numBlocks; i++) {
+        for (int i = freeList; i < totalBlocks; i++) {
             byte[] tempBytes = new byte[BLOCK_SIZE];
 
             SysLib.int2bytes(i + 1, tempBytes, 0);
@@ -58,8 +61,8 @@ public class SuperBlock {
     public void sync(){
         byte[] blockData = new byte[BLOCK_SIZE];
 
-        SysLib.int2bytes(numBlocks, blockData, BLOCK_LOC);
-        SysLib.int2bytes(numInodes, blockData, INODE_LOC);
+        SysLib.int2bytes(totalBlocks, blockData, BLOCK_LOC);
+        SysLib.int2bytes(inodeBlocks, blockData, INODE_LOC);
         SysLib.int2bytes(freeList, blockData, FREELIST_LOC);
 
         SysLib.rawread(BLOCK_LOC, blockData);
