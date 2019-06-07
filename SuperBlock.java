b2951fb14a3643dc5d7e165ec927dd3833d44dd3
Aaron <aaronjameshays@gmail.com>
2019-06-07 12:29:03 -0700

    Superblock beginning

---
+++ SuperBlock.java
@@ -0,0 +1,20 @@
+/**
+ * Created by
+ * Aaron
+ * 2019-06-07
+ */
+
+public class SuperBlock {
+
+    private final int INODE_BLOCKS = 64;
+    private final int BLOCK_SIZE = Disk.blockSize;
+    private final int INODE_LOC = 4;
+    private final int BLOCK_LOC = 0;
+    priva
+
+
+    public SuperBlock(int totalBlocks){
+
+    }
+
+}
