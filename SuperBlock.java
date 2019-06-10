100585bf89a9363026e4a9abe19fe86b456c322c
jbrown725 <jbrown725@gmail.com>
2019-06-09 17:48:46 -0700

    comments

--- SuperBlock.java
+++ SuperBlock.java
@@ -1,44 +1,51 @@
-/**
- * Created by
- * Aaron
- * 2019-06-07
+/** SuperBlock.java
+ *Name: Jordan Brown and Aaron Hays
+ * Class: CSS 430
+ * Project: P5
+ * Description: The SuperBlock is a file containing metadata of the filesystem.
+ * It is represented on-disk as block 0
+ *
+ *
  */
 
 public class SuperBlock {
 
-    private final int NUM_OF_INODE_BLOCKS = 64;    // total no of inode blocks
-    private final int BLOCK_SIZE = Disk.blockSize;  // block size from Disk class
+    private final int NUM_OF_INODE_BLOCKS = 64;    // total num of inode blocks
+    private final int BLOCK_SIZE = Disk.blockSize; // block size from Disk
     private final int INODE_LOC = 4;    // location of first inode
     private final int BLOCK_LOC = 0;    // location of superblock
     private final int FREELIST_LOC = 8; // location of freelist
 
-    public int inodeBlocks;
-    public int totalBlocks;
-    public int freeList;
+    public int inodeBlocks;     // number of inodes
+    public int totalBlocks;     // number of disk blocks
+    public int freeList;        // the block number of the free list's head
 
-
-
-
-
-    public SuperBlock(int totalBlocks){
+    /*---------------------------------Constructor-----------------------------*/
+    // Constructs a SuperBlock and sets the totalBlocks inodeBlocks,
+    // and freeList member variables.
+    public SuperBlock(int blockNum){
 
         byte[] superBlock = new byte[BLOCK_SIZE];
         SysLib.rawread(BLOCK_LOC, superBlock);
-        this.totalBlocks = SysLib.bytes2int(superBlock, BLOCK_LOC);
+        totalBlocks = SysLib.bytes2int(superBlock, BLOCK_LOC);
         inodeBlocks = SysLib.bytes2int(superBlock, INODE_LOC);
         freeList = SysLib.bytes2int(superBlock, FREELIST_LOC);
 
-
-        if(this.totalBlocks != totalBlocks || inodeBlocks <= 0 || freeList < 2){
-            this.totalBlocks = totalBlocks;
-            format(NUM_OF_INODE_BLOCKS);
+        // if disk is invalid, format
+        if(totalBlocks != blockNum || inodeBlocks <= 0 || freeList < 2){
+            totalBlocks = blockNum;
+            format();
         }
     }
 
-//    public void format(){
-//        format(64);
-//    }
+    /*----------------------------------format-------------------------------*/
+    // Default format formats default number of inode blocks
+    public void format(){
+        format(NUM_OF_INODE_BLOCKS);
+    }
 
+    /*----------------------------------format-------------------------------*/
+    // Removes all data from disk and resets member variables
     public void format(int blockNum){
 
         inodeBlocks = blockNum;
@@ -65,6 +72,9 @@ public class SuperBlock {
         sync();
     }
 
+    /*------------------------------------sync-------------------------------*/
+    // Syncs the SuperBlock on disk with this SuperBlock object to write any
+    // new updates to member variables
     public void sync(){
         byte[] blockData = new byte[BLOCK_SIZE];
 
@@ -72,27 +82,39 @@ public class SuperBlock {
         SysLib.int2bytes(inodeBlocks, blockData, INODE_LOC);
         SysLib.int2bytes(freeList, blockData, FREELIST_LOC);
 
-        SysLib.rawread(BLOCK_LOC, blockData);
+        SysLib.rawwrite(BLOCK_LOC, blockData);
     }
 
+    /*---------------------------------getFreeBlock--------------------------*/
+    // Returns the first free block from the freeList
     public int getFreeBlock(){
 
         int freeBlock = freeList;
-        if(freeBlock > 0){
+
+        // Check that the free block is in valid range
+        if(freeBlock > 0 && freeBlock < totalBlocks){
             byte[] blockData = new byte[BLOCK_SIZE];
             SysLib.rawread(freeList, blockData);
 
+            // update the free block
             freeList = SysLib.bytes2int(blockData, BLOCK_LOC);
+
+            // update the new bree block with the old's data
             SysLib.int2bytes(BLOCK_LOC, blockData, BLOCK_LOC);
             SysLib.rawwrite(freeBlock, blockData);
         }
         return freeBlock;
     }
 
+    /*--------------------------------returnBlock----------------------------*/
+    // Adds a newly freed block back to the free list by setting the block at
+    // blockNum to be the new free block and making the old free block to be
+    // the next free block in the list
     public boolean returnBlock(int blockNum){
         if(blockNum < 0){
             return false;
         }
+
         byte[] blockData = new byte[BLOCK_SIZE];
         for (int i = 0; i < BLOCK_SIZE; i++) {
             blockData[i] = 0;
@@ -100,6 +122,7 @@ public class SuperBlock {
 
         SysLib.int2bytes(freeList, blockData, BLOCK_LOC);
         SysLib.rawwrite(blockNum, blockData);
+
         freeList = blockNum;
         return true;
     }
